9c770b8afe82da650d3529e41157f012f9386857
Froweey <lottasay@gmail.com>
2017-05-29 02:15:12 -0700

    build #2: code tidy up

--- feature_requests/utils.py
+++ feature_requests/utils.py
@@ -9,25 +9,25 @@ def dump_datetime(value):
     return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
 
 def serialize(model):
-	"""
-	Serializes a Flask SQLAlchemy model object into JSON.
-	"""
-	json = {}
-	if type(model) == dict:
-		data_type = model
-	else:
-		data_type = model.__dict__
-	for key, value in data_type.iteritems():
-		_type = type(value)
-		if _type == datetime:
-			json[key] = dump_datetime(value)
-		elif _type == Choice:
-			json[key] = {
-			    "code": value.code,
-			    "value": value.value
-			}
-		elif _type == InstanceState:
-			continue
-		else:
-			json[key] = value
-	return json
+    """
+    Serializes a Flask SQLAlchemy model object into JSON.
+    """
+    json = {}
+    if type(model) == dict:
+        data_type = model
+    else:
+        data_type = model.__dict__
+    for key, value in data_type.iteritems():
+        _type = type(value)
+        if _type == datetime:
+            json[key] = dump_datetime(value)
+        elif _type == Choice:
+            json[key] = {
+                "code": value.code,
+                "value": value.value
+            }
+        elif _type == InstanceState:
+            continue
+        else:
+            json[key] = value
+    return json
